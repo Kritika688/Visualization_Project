418bf84c215136a3799a4b2c1bcb5583939a26e7
Kritika <kritika@bu.edu>
2015-04-23 12:19:35 -0400

    Final Project
    Final Data Visualization

---
+++ FINAL/PHP/test3.php
@@ -0,0 +1,37 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$node2 = $_POST['node'];	
+$node2 = $node2[0];
+
+$sql = "SELECT attribute, attribute_value FROM node_table WHERE id LIKE '".$node2."'";
+$result2 = $conn->query($sql);
+
+$info_output = '<br>';
+
+for($x = 0 ; $x < $result2->num_rows ; $x++)
+{
+	$row1 = mysqli_fetch_assoc($result2);
+	if($row1["attribute"] != "x" && $row1["attribute"] != "y")
+	{
+		$info_output .=  $row1["attribute"] ;
+		$info_output .= ' : ';
+		$info_output .=  $row1["attribute_value"] ;
+		$info_output .= '<br>';
+	}
+}
+
+echo $info_output;
+$conn->close();
+?> 
